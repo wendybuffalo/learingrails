8742b9126b79f8721315708bc022c74219b2f1d7
vagrant <vagrant@learning-rails>
2013-08-25 18:17:50 +0000

    add root route to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Microposts::Application.routes.draw do
   
   devise_for :users
-  
+
   resources :posts
 
   resources :users
@@ -10,7 +10,7 @@ Microposts::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
